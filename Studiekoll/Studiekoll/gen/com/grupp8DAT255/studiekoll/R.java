47154a3ca2a45151242af68d01203131f610c645
dpBitter <dpbitter@gmail.com>
2014-05-16 10:25:57 +0200

    initial help text commit

--- Studiekoll/Studiekoll/gen/com/grupp8DAT255/studiekoll/R.java
+++ Studiekoll/Studiekoll/gen/com/grupp8DAT255/studiekoll/R.java
@@ -1159,7 +1159,7 @@ containing a value of this type.
         public static final int button_help=0x7f0a0012;
         public static final int button_input=0x7f0a0011;
         public static final int edit_message=0x7f0a000f;
-        public static final int hello_world=0x7f0a0014;
+        public static final int help_text=0x7f0a0014;
         public static final int title_activity_help=0x7f0a0013;
     }
     public static final class style {
