3d079de061681bf3cda329f1dd9e68de1ea959d6
dpBitter <dpbitter@gmail.com>
2014-05-16 12:20:01 +0200

    Inget nytt

--- Studiekoll/Studiekoll/gen/com/grupp8DAT255/studiekoll/R.java
+++ Studiekoll/Studiekoll/gen/com/grupp8DAT255/studiekoll/R.java
@@ -999,7 +999,7 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button1=0x7f05003e;
+        public static final int button1=0x7f05003f;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1013,6 +1013,7 @@ containing a value of this type.
         public static final int expanded_menu=0x7f05002c;
         public static final int graphics_button=0x7f050040;
         public static final int help_button=0x7f050042;
+        public static final int help_text_view=0x7f05003e;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
@@ -1046,7 +1047,6 @@ containing a value of this type.
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
         public static final int textView1=0x7f05003d;
-        public static final int textView2=0x7f05003f;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
